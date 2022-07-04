ac1617142ac00add0c334e365e1de2eb106221ad
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-07-04 11:16:30 +0900

    fix

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -488,6 +488,7 @@ const Home: NextPage = () => {
 
   const pointerMove = (e: any) => {
     if (!isDrag) { return }
+    alert("aaa")
     console.log(e.pressure)
     console.log("これみたい", e);
     if (e.pressure != 0) {
