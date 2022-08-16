7d879e23faf6b1d83032d8518782e883cbbc9de0
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-16 13:54:01 +0900

    add: ページ遷移

--- client/components/common/BackButton.tsx
+++ client/components/common/BackButton.tsx
@@ -12,7 +12,7 @@ const BackButton: NextPage<Props> = (props) => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-blue-400 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
         </svg>
-        <p className='text-blue-400 text-sm my-auto'>{buttonText}</p>
+        <a href="/" className='text-blue-400 text-sm my-auto'>{buttonText}</a>
       </button>
     </>
 	);
