80dff22c9b50b8044a466788bbc5127f157b83b6
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-20 23:22:46 +0900

    add: connection refusedへの対応処理

--- api/util/db.go
+++ api/util/db.go
@@ -1,12 +1,15 @@
 package util
 
 import (
+	"fmt"
 	"os"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var Db gorm.DB
+var Db *gorm.DB
 
 func InitDb() *gorm.DB {
 	USER := os.Getenv("MYSQL_USER")
@@ -15,11 +18,28 @@ func InitDb() *gorm.DB {
 	DBNAME := os.Getenv("MYSQL_DATABASE")
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-	Db, err := gorm.Open(mysql.Open(CONNECT), &gorm.Config{})
+
+	dialector := mysql.Open(CONNECT)
+
+	Db, err := gorm.Open(dialector, &gorm.Config{})
 	if err != nil {
-		panic(err.Error())
+		WaitConnect(dialector, 100)
 	}
 	Db.Set("gorm:table_options", "ENGINE=InnoDB")
 
 	return Db
+}
+
+func WaitConnect(dialector gorm.Dialector, count uint) {
+	var err error
+	if Db, err = gorm.Open(dialector); err != nil {
+		if count > 1 {
+			time.Sleep(time.Second * 2)
+			count--
+			fmt.Println("retry_count: ", 100-count)
+			WaitConnect(dialector, count)
+			return
+		}
+		panic(err.Error())
+	}
 }
