8a2481af0d2818ac38c9efd0e4b939876bfc96a1
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-21 18:29:49 +0900

    add: 開発環境と本番環境でのAPIURLを定義

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const dotenv = require('dotenv');
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+dotenv.config();
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    API_URL: IS_PRODUCTION ? process.env.API_URL_PROD : process.env.API_URL_DEV,
+  },
+};
