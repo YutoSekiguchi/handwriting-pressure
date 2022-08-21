22144fee87bb35d4ace8fdf11ea3aeaede66816f
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-21 16:47:40 +0900

    add

---
+++ client/hooks/reducers/usersReducer.ts
@@ -0,0 +1,54 @@
+import { usersActions } from "../actions/usersActions";
+
+export const initialState = {
+  fetching: false,
+  userAllData: [],
+  error: null
+}
+
+export function usersReducer(state: any, actions: any) {
+  switch (actions.type) {
+    case usersActions.CREATE_EXAM_USER:
+      return {
+        ...state,
+        fetching: true,
+      }
+
+    case usersActions.CREATE_EXAM_USER_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        ok: true,
+      }
+
+    case usersActions.CREATE_EXAM_USER_ERROR:
+      return {
+        ...state,
+        fetching: false,
+        error: actions.payload
+      }
+    
+    case usersActions.GET_ALL_EXAM_USERS:
+      return {
+        ...state,
+        fetching: true
+      }
+    
+    case usersActions.GET_ALL_EXAM_USERS_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        userAllData: actions.payload
+      }
+    
+    case usersActions.GET_ALL_EXAM_USERS_ERROR:
+      return {
+        ...state,
+        fetching: false,
+        error: actions.payload
+      }
+    
+    default:
+      return state
+  }
+}
