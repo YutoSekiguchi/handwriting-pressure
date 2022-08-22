5de1aee929114cfc0c1e9bb6d91c91b8afd71bb3
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-22 15:34:36 +0900

    fix: 戻り値をnullに修正

--- api/service/exam_user.go
+++ api/service/exam_user.go
@@ -19,23 +19,29 @@ func (s ExamUserService) GetExamUserList(db *gorm.DB) ([]ExamUser, error) {
 }
 
 // 名前とパスワードからユーザを取得
-func (s ExamUserService) GetExamUserByNameAndPwd(db *gorm.DB, c echo.Context) (ExamUser, error) {
-	var u ExamUser
+func (s ExamUserService) GetExamUserByNameAndPwd(db *gorm.DB, c echo.Context) (*ExamUser, error) {
+	u := new(ExamUser)
 	name := c.QueryParam("Name")
 	password := c.QueryParam("Password")
 	if err := db.Raw("SELECT * FROM `exam_users` WHERE name = ? AND password = ? LIMIT 1", name, password).Scan(&u).Error; err != nil {
 		return u, err
 	}
+	if (u.ID == 0) {
+		return nil, nil
+	}
 	return u, nil
 }
 
 // idからユーザを取得
-func (s ExamUserService) GetExamUserByID(db *gorm.DB, c echo.Context) (ExamUser, error) {
-	var u ExamUser
+func (s ExamUserService) GetExamUserByID(db *gorm.DB, c echo.Context) (*ExamUser, error) {
+	u := new(ExamUser)
 	id := c.Param("id")
 
 	if err := db.Raw("SELECT * FROM `exam_users` WHERE id = ? LIMIT 1", id).Scan(&u).Error; err != nil {
 		return u, err
+	}
+	if (u.ID == 0) {
+		return nil, nil
 	}
 	return u, nil
 }
