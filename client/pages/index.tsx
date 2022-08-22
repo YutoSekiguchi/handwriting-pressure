fad9e2b6f54801cb10f2c99dd67c28c8a7828631
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-23 02:27:16 +0900

    add

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next'
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Head from 'next/head'
 import AppHeader from '../components/common/AppHeader';
 
 const Home: NextPage = () => {
-
 	return (
     <>
-      <div className='fixed bg-gray-900 w-full h-full'>
+      <div className='fixed w-full h-full bg-gray-900'>
         <AppHeader />
         <h1>HOME</h1>
         <h1>HOME</h1>
