dfb9466668470d312f8c6d55a981f6417291f5d1
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-23 02:26:12 +0900

    add: AppProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,14 +1,14 @@
 import '../styles/globals.css'
 import '../styles/Slider.css'
 import type { AppProps } from 'next/app'
-import { UsersProvider } from '../hooks/contexts/usersContext'
+import AppProvider from '../hooks/contexts/main'
 // import "../styles/my-custom-class.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <UsersProvider>
+    <AppProvider>
       <Component {...pageProps} />
-    </UsersProvider>
+    </AppProvider>
   </>
 }
 
