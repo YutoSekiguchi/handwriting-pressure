c02cc6fa8c5361ffe82a38cd72449b8467df61a8
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-27 12:47:01 +0900

    remove: not use css

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import '../styles/Slider.css'
 import type { AppProps } from 'next/app'
 import AppProvider from '../hooks/contexts/main'
-// import "../styles/my-custom-class.css"
 import lscache from 'lscache';
 import { useEffect } from 'react';
 
