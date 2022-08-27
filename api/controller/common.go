bd92d496cf095f025e04590d4d81d4c324a5b731
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-27 22:24:33 +0900

    fix: Controller毎回同じようなこと書くのめんどくさいかったから書き方変えた

--- api/controller/common.go
+++ api/controller/common.go
@@ -1,6 +1,12 @@
 package controller
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
 
 // Controller Struct
 type Controller struct {
@@ -10,5 +16,14 @@ type Controller struct {
 func NewController(db *gorm.DB) *Controller {
 	return &Controller{
 		Db: db,
+	}
+}
+
+func Res(c echo.Context, p interface{}, err error) error {
+	if err != nil {
+		fmt.Println(err)
+		return c.JSON(http.StatusNotFound, err.Error())
+	} else {
+		return c.JSON(200, p)
 	}
 }
