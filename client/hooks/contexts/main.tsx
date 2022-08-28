719c1b233b8563d8a94cdabffa28f0869491cdca
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-08-28 16:40:40 +0900

    add: context

--- client/hooks/contexts/main.tsx
+++ client/hooks/contexts/main.tsx
@@ -1,10 +1,19 @@
 import React, { ReactNode } from 'react'
+import { LogsProvider } from './logsContext';
+import { PaperDetailsProvider } from './paperDetailsContext';
+import { PapersProvider } from './papersContext';
 import { UsersProvider } from './usersContext'
 
 const AppProvider = (props: { children: ReactNode }) => {
   return (
     <UsersProvider>
-      {props.children}
+      <LogsProvider>
+        <PapersProvider>
+          <PaperDetailsProvider>
+            {props.children}
+          </PaperDetailsProvider>
+        </PapersProvider>
+      </LogsProvider>
     </UsersProvider>
   )
 }
