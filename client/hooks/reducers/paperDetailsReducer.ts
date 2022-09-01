95d0f10f1fecac23c70dc26f72763795c410abe6
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-09-01 17:55:18 +0900

    add: getPaperDetailByID

--- client/hooks/reducers/paperDetailsReducer.ts
+++ client/hooks/reducers/paperDetailsReducer.ts
@@ -29,6 +29,27 @@ export const papersDetailReducer = (state: any, actions: any) => {
         fetching: false,
         error: actions.payload
       }
+
+    case papersDetailActions.CREATE_PAPER_DETAIL:
+      return {
+        ...state,
+        fetching: true,
+      }
+
+    case papersDetailActions.CREATE_PAPER_DETAIL_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        paperDetail: actions.payload,
+        ok: true,
+      }
+    
+    case papersDetailActions.CREATE_PAPER_DETAIL_ERROR:
+      return {
+        ...state,
+        fetching: false,
+        error: actions.payload
+      }
     
     case papersDetailActions.GET_PAPER_DETAILS:
       return {
