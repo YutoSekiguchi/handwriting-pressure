0c996a398e4684040f89e48475ca679a24e31741
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-09-10 16:52:46 +0900

    add: 並行処理 paper削除api

--- api/service/paper.go
+++ api/service/paper.go
@@ -31,14 +31,51 @@ func (s PaperService) PostPaper(db *gorm.DB, c echo.Context) (Paper, error) {
 	return paper, nil
 }
 
+type PaperDetailID struct {
+	ID int `gorm:"column:id"`
+}
+
 // DELETE
 // paperの削除
 func (s PaperService) DeletePaper(db *gorm.DB, c echo.Context) ([]Paper, error) {
 	var p []Paper
+	var pdidList []PaperDetailID
+	var pd []PaperDetail
+	var log []Log
+	var stroke []Stroke
+	var error error
+	error = nil
 	id := c.Param("id")
 
 	if err := db.Where("id = ?", id).Delete(&p).Error; err != nil {
 		return nil, err
+	}
+	if err := db.Raw("SELECT paper_details.id FROM `paper_details` WHERE pid = ?", id).Scan(&pdidList).Error; err != nil {
+		return nil, err
+	}
+	for i := 0; i < len(pdidList); i++ {
+		pdid := pdidList[i].ID
+
+		go func() {
+			if err := db.Where("id = ?", pdid).Delete(&pd).Error; err != nil {
+				error = err
+			}
+		}()
+
+		go func() {
+			if err := db.Where("pdid = ?", pdid).Delete(&stroke).Error; err != nil {
+				error = err
+			}
+		}()
+
+		go func() {
+			if err := db.Where("pdid = ?", pdid).Delete(&log).Error; err != nil {
+				error = err
+			}
+		}()
+	}
+	if (error != nil) {
+		return nil, error
 	}
 	return p, nil
 }
