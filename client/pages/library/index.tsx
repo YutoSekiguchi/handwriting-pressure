53944c02b733e76cda17e30d7bb2f38543f5dab9
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-09-12 12:38:35 +0900

    説明画面のコンポーネント化

--- client/pages/library/index.tsx
+++ client/pages/library/index.tsx
@@ -12,6 +12,7 @@ import NewNoteDialog from '../../components/library/NewNoteDialog';
 import ShowUserName from '../../components/library/ShowUserName';
 import FolderList from '../../components/library/FolderList';
 import InputNewFolder from '../../components/library/InputNewFolder';
+import Explanation from '../../components/library/Explanation';
 
 type FolderObj = {
   ID: number,
@@ -257,24 +258,7 @@ const Library: NextPage = () => {
           <div className='w-5/6 h-full bg-gray-800'>
             <div className='flex-col h-full mx-6 mt-16'>
               {!openFolderIndexAndPID&&
-                <div className='h-full mt-12 text-center'>
-                  <h3 className='justify-around font-bold text-white'>〜ノートライブラリ〜</h3>
-                  <div className='flex justify-center py-6 mt-12 bg-gray-900 border-8 border-white h-1/3 rounded-3xl'>
-                    <img src={'/make_folder.png'} className="object-contain w-1/4" />
-                    <div className='flex-col w-1/2 my-auto ml-12'>
-                      <h4 className='mb-3 text-white'>〜フォルダ作成方法〜</h4>
-                      <h4 className='text-left text-white'>&emsp;ユーザ名の横にあるプラスボタンをクリックして，フォルダの名前を入力することで，新しいフォルダを作成することができます</h4>
-                    </div>
-                  </div>
-
-                  <div className='flex justify-center py-6 mt-12 bg-gray-900 border-8 border-white rounded-3xl h-1/3'>
-                    <img src={'/make_note.png'} className="object-contain w-1/4" />
-                    <div className='flex-col w-1/2 my-auto ml-12'>
-                      <h4 className='mb-3 text-white'>〜ノート作成方法〜</h4>
-                      <h4 className='text-left text-white'>&emsp;このプラスマークを押してノートの名前を入力することで，新規ノートを作成できます</h4>
-                    </div>
-                  </div>
-                </div>
+                <Explanation />
               }
 
               {openFolderIndexAndPID&&openFolderIndexAndPID.index>=0&&
