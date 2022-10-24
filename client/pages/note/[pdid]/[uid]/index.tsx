ef5de127c144e0b35cd7491f2446f1432f9b2082
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-10-24 14:40:07 +0900

    ノートの最後の文字が"_no"だったらUIを非表示に

--- client/pages/note/[pdid]/[uid]/index.tsx
+++ client/pages/note/[pdid]/[uid]/index.tsx
@@ -791,7 +791,7 @@ const Note: NextPage = () => {
         />
         
         {/* 操作UI */}
-        {(paperDetails.state.paperDetail.Title&&paperDetails.state.paperDetail.Title.slice(0, 2) != "no")?
+        {(paperDetails.state.paperDetail.Title&&paperDetails.state.paperDetail.Title.slice(-3) != "_no")?
         <div className='fixed right-0 w-4/12 h-full bg-gray-900 top-12'>
           <div className='w-11/12 mx-auto mt-2 text-center bg-gray-800 h-1/3 rounded-3xl'>
             <h3 className='pt-3 font-bold text-white'>Undo/Redo&nbsp;<span onClick={() => setShowExplainDialog(4)}><QuestionMarkButton /></span></h3>
