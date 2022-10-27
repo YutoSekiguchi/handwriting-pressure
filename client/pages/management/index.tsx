276ad9619ddeee6ffbb35b8f7f230201ab961947
YutoSekiguchi <yuutosekiguchi@gmail.com>
2022-10-27 19:30:27 +0900

    add

--- client/pages/management/index.tsx
+++ client/pages/management/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import React, { useEffect, useRef, useState } from 'react';
 import AppHeader from '../../components/common/AppHeader';
+import { usePaperDetails } from '../../hooks/contexts/paperDetailsContext';
 import { useUsers } from '../../hooks/contexts/usersContext';
 
 const ManagementPage: NextPage = () => {
   const [checkedItems, setCheckedItems] = useState({})
   const canvasRef = useRef(null);
   const users: any = useUsers();
+  const paperDetails: any = usePaperDetails();
 
   type ExamUserObj = {
     Name: string,
@@ -20,6 +22,7 @@ const ManagementPage: NextPage = () => {
       ...checkedItems,
       [e.target.name]: e.target.checked
     })
+    console.log(paperDetails);
   }
 
   useEffect(() => {
@@ -28,18 +31,19 @@ const ManagementPage: NextPage = () => {
 
   useEffect(() => {
     users.getAllExamUsers();
-    
+    paperDetails.getPaperDetailsWithPressureUndo();
+    paperDetails.getPaperDetailsWithNotPressureUndo();
   }, [])
 
   return (
     <>
-      <div className='fixed w-full h-full bg-gray-500'>
+      <div className='fixed w-full h-full bg-gray-500 overflow-auto'>
         <AppHeader />
         <h2 className='mt-12 text-white font-bold text-center'>管理者ツール</h2>
         <div className='w-3/4 mx-auto bg-gray-800 h-3/4 rounded-xl pt-6 mt-12'>
 
         </div>
-        <div className='w-1/2 mx-auto bg-gray-800 h-1/2 rounded-xl pt-6 mt-12'>
+        <div className='w-1/2 mx-auto bg-gray-800 h-1/2 rounded-xl pt-6 my-12'>
           <h3 className="text-white font-bold text-center">ユーザごとの分析</h3>
           <div className='flex justify-center overflow-auto'>
             {
